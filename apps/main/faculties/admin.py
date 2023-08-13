e8a566127ad9edb408fb4c190b430dbc1dad92d4
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-08-13 16:04:48 +0500

    Added admin panel for Faculty

--- apps/main/faculties/admin.py
+++ apps/main/faculties/admin.py
@@ -1,3 +1,13 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Project
+from apps.main.faculties.models import Faculty
+
+
+@admin.register(Faculty)
+class FacultyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'program', 'dean']
+    list_display_links = ['name', 'program']
+    search_fields = ['name', ]
+    ordering = ['-id']
