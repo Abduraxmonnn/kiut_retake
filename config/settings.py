ca4600ab2c85b9d4760dad2d16978f88bb485aec
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-12-20 20:02:01 +0500

    Added PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -102,12 +102,25 @@ AUTH_USER_MODEL = 'user.User'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DB_SQLITE = "sqlite"
+DB_POSTGRESQL = "postgresql"
+
+DATABASES_ALL = {
+    DB_SQLITE: {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    DB_POSTGRESQL: {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'retake_db',
+        'USER': 'retake_user',
+        'PASSWORD': 'retake1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+DATABASES = {"default": DATABASES_ALL[os.environ.get("BACKEND_DB")]}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
